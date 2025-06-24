6da40934e39d9541f4d747924eaab59db92e613d
pwinning1991 <ftcpjw@yahoo.com>
2025-06-24 09:54:20 -0400

    update when player is drawn

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
         player.draw(screen)
+        pygame.display.flip()
         dt = clock.tick(60) / 1000.0
 
 
